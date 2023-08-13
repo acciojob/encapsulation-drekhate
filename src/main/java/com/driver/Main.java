3aae872efd46cf6a4122c8ae7274c33544bc86f2
Dipak Rekhate <drekhate@gmail.com>
2023-08-13 09:55:07 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        System.out.println(obj.name);
+//        name has private access in com.driver.RWOnly
+        obj.setName("Dipak");
+        String name = obj.getName();
+        System.out.println(name);
+    }
   
 }
